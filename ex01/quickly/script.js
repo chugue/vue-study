faeb78087b97c25ab5dd1c2f87626c3c81405bd5
chugue <chugue85@gmail.com>
2024-08-12 21:02:13 +0900

    vue study

---
+++ ex01/quickly/script.js
@@ -0,0 +1,18 @@
+let btn = document.querySelector("#btn");
+btn.addEventListener("click", function () {
+  alert("Hello World!");
+});
+
+// window.onload = function () {
+//     let btn = document.querySelector("#btn");
+//     btn.addEventListener("click", function () {
+//         alert("Hello world!");
+//   });
+// };
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   let btn = document.querySelector("#btn");
+//   btn.addEventListener("click", function () {
+//     alert("Hello world!");
+//   });
+// });
---
+++ ex01/src/components/AppView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <section>
+      <book-component></book-component>
+      <book-component></book-component>
+      <book-component></book-component>
+      <book-component></book-component>
+      <book-component></book-component>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
