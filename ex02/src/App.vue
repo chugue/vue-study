640e033f1e339faa9f99611f6822369f7e1f5482
chugue <chugue85@gmail.com>
2024-08-13 23:19:31 +0900

    lecture destructure

--- ex02/src/App.vue
+++ ex02/src/App.vue
@@ -1,11 +1,29 @@
 <template>
-	<div></div>
+	<div>
+		<h2>author: {{ author }}</h2>
+		<h2>title: {{ title }}</h2>
+	</div>
 </template>
 
 <script>
+import { reactive, toRefs } from 'vue';
 export default {
 	setup() {
-		return {};
+		const book = reactive({
+			author: 'Vue Team',
+			year: '2020',
+			title: 'Vue 3 Guide',
+			description: '당신은 지금 바로 이 책을 읽습니다.',
+			price: '무료',
+		});
+
+		const { author, title } = toRefs(book);
+
+		return {
+			author,
+			title,
+			book,
+		};
 	},
 };
 </script>
