02e3fe670a6a977f783bc26ce5db302903d3584c
chugue <chugue85@gmail.com>
2024-08-15 23:01:20 +0900

    ex14

--- ex02/.eslintrc.cjs
+++ ex02/.eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
 	},
 	rules: {
 		'no-console': process.env.NODE_ENV === 'prod' ? 'error' : 'off',
+		'no-unused-vars': 'off',
 		'prettier/prettier': [
 			'error',
 			{
---
+++ ex02/src/lectures/ex09/App_Class_Binding.vue
@@ -0,0 +1,53 @@
+<template>
+	<div>
+		<div class="text" :class="[isActive ? 'active-class' : 'class', ErrorClass]">
+			텍스트 입니다.
+		</div>
+		<button @click="toggle">toggle</button>
+		<button @click="hasError = !hasError">toggle Error</button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+		const hasError = ref(true);
+
+		const ClassObject = computed(() => {
+			return {
+				active: true,
+				'text-danger': true,
+			};
+		});
+
+		const ActiveClass = ref('active');
+		const ErrorClass = ref('error');
+
+		return {
+			toggle,
+			isActive,
+			hasError,
+			ClassObject,
+			ActiveClass,
+			ErrorClass,
+		};
+	},
+};
+</script>
+
+<style scoped>
+.active {
+	font-weight: 900;
+	font-size: 50px;
+}
+.text-danger {
+	color: red;
+}
+</style>
