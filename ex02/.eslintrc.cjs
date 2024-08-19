7327fb3ad1189dd66c6d0fbcdfce9078340c1430
AI_Nomads <chugue85@gmail.com>
2024-08-20 00:09:37 +0900

    ex15

--- ex02/.eslintrc.cjs
+++ ex02/.eslintrc.cjs
@@ -1,36 +1,36 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution');
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-	root: true,
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: [
-		'plugin:vue/vue3-essential',
-		'eslint:recommended',
-		'@vue/eslint-config-prettier/skip-formatting',
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-	},
-	rules: {
-		'no-console': process.env.NODE_ENV === 'prod' ? 'error' : 'off',
-		'no-unused-vars': 'off',
-		'prettier/prettier': [
-			'error',
-			{
-				singleQuote: true,
-				semi: true,
-				useTabs: true,
-				tabWidth: 1,
-				trailingComma: 'all',
-				printWidth: 80,
-				bracketSpacing: true,
-				arrowParens: 'avoid',
-			},
-		],
-	},
-};
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'prod' ? 'error' : 'off',
+    'no-unused-vars': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid'
+      }
+    ]
+  }
+}
